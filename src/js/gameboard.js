c6d283e692fb5ee0757676e16f3a6fccbc619e61
Tzun Wong <tzun.wong@gmail.com>
2022-05-29 14:01:22 +0800

    added and tested receiveAttack() & checkPlaceShipValid() functions

--- src/js/gameboard.js
+++ src/js/gameboard.js
@@ -2,14 +2,27 @@ import Ship from "./ships";
 
 export default class Gameboard {
   constructor() {
-    // keys are coordinates, values are ship in this.shipsDatabase
+    // keys are coordinates, values are {isAttacked: boolean, ship: this.shipsDatabase.name}
     this.boardDatabase = {};
     // keys are ship name, values are Ship class objects
     this.shipsDatabase = {};
   }
 
+  // only works in empty gameboard
+  checkPlaceShipValid(newShipInputs) {
+    const areGridsEmpty = newShipInputs.coordinates.every((position) => {
+      return !(position in this.boardDatabase);
+    });
+    return areGridsEmpty;
+  }
+
   // shipData {name: '', coordinates: ['x1y1', ...]}
   placeShip(newShipInputs) {
+    // catch placement on occupied grids
+    if (this.checkPlaceShipValid(newShipInputs) == false) {
+      return "invalid placement";
+    }
+
     let newShip = new Ship(newShipInputs);
 
     // adds newShip to shipsDatabase
@@ -19,5 +32,26 @@ export default class Gameboard {
     newShipInputs.coordinates.forEach((position) => {
       this.boardDatabase[position] = { ship: this.shipsDatabase[newShipInputs.name] };
     });
+  }
+
+  receiveAttack(position) {
+    // grid not attacked && no ship
+    if (position in this.boardDatabase == false) {
+      this.boardDatabase[position] = { isAttacked: true };
+      return "miss";
+    }
+
+    let grid = this.boardDatabase[position];
+
+    // grid not attacked && ship
+    if ("isAttacked" in grid == false && "ship" in grid == true) {
+      grid.isAttacked = true;
+      grid.ship.hit(position);
+      return "hit";
+    }
+    // grid attacked already, with ship or no ship
+    if (grid.isAttacked == true) {
+      return "error";
+    }
   }
 }
--- src/js/tests/gameboard.test.js
+++ src/js/tests/gameboard.test.js
@@ -1,9 +1,30 @@
 import Gameboard from "../gameboard";
 import Ship from "../ships";
 
+test("check place ship valid", () => {
+  const gameboard = new Gameboard();
+  const shipData1 = { name: "ship1", coordinates: ["x1y1", "x2y1", "x3y1"] };
+
+  expect(gameboard.checkPlaceShipValid(shipData1)).toEqual(true);
+});
+
+test("check place ship invalid", () => {
+  const gameboard = new Gameboard();
+  const shipData1 = { name: "ship1", coordinates: ["x1y1", "x2y1", "x3y1"] };
+
+  expect(gameboard.checkPlaceShipValid(shipData1)).toEqual(true);
+
+  gameboard.placeShip(shipData1);
+
+  expect(gameboard.checkPlaceShipValid(shipData1)).toEqual(false);
+});
+
 test("gameboard placeship", () => {
   const gameboard = new Gameboard();
   const shipData1 = { name: "ship1", coordinates: ["x1y1", "x2y1", "x3y1"] };
+
+  gameboard.placeShip(shipData1);
+
   const expectedShip = new Ship(shipData1);
   const expectedBoard = {
     x1y1: { ship: expectedShip },
@@ -12,8 +33,112 @@ test("gameboard placeship", () => {
   };
   const expectedShips = { ship1: expectedShip };
 
-  gameboard.placeShip(shipData1);
-
   expect(gameboard.shipsDatabase).toEqual(expectedShips);
   expect(gameboard.boardDatabase).toEqual(expectedBoard);
+});
+
+test("gameboard placeship invalid", () => {
+  const gameboard = new Gameboard();
+  const shipData1 = { name: "ship1", coordinates: ["x1y1", "x2y1", "x3y1"] };
+  const shipData2 = { name: "ship2", coordinates: ["x1y1", "x1y2", "x1y3"] };
+
+  gameboard.placeShip(shipData1);
+
+  const expectedShip = new Ship(shipData1);
+  const expectedBoard = {
+    x1y1: { ship: expectedShip },
+    x2y1: { ship: expectedShip },
+    x3y1: { ship: expectedShip },
+  };
+  const expectedShips = { ship1: expectedShip };
+
+  expect(gameboard.placeShip(shipData2)).toEqual("invalid");
+  expect(gameboard.shipsDatabase).toEqual(expectedShips);
+  expect(gameboard.boardDatabase).toEqual(expectedBoard);
+});
+
+test("gameboard recieve attack hit ship", () => {
+  let gameboard = new Gameboard();
+  let shipData1 = { name: "ship1", coordinates: ["x1y1", "x2y1", "x3y1"] };
+  const attackCoordinate = "x1y1";
+
+  gameboard.placeShip(shipData1);
+
+  const expectedShip = new Ship(shipData1);
+  expectedShip.hit(attackCoordinate);
+  const expectedBoard = {
+    x1y1: { isAttacked: true, ship: expectedShip },
+    x2y1: { ship: expectedShip },
+    x3y1: { ship: expectedShip },
+  };
+  const expectedShips = { ship1: expectedShip };
+
+  expect(gameboard.receiveAttack("x1y1")).toEqual("hit");
+  expect(gameboard.boardDatabase).toEqual(expectedBoard);
+  expect(gameboard.shipsDatabase).toEqual(expectedShips);
+});
+
+test("gameboard recieve attack hit empty", () => {
+  let gameboard = new Gameboard();
+  let shipData1 = { name: "ship1", coordinates: ["x1y1", "x2y1", "x3y1"] };
+  let attackCoordinate = "x1y2";
+
+  gameboard.placeShip(shipData1);
+
+  const expectedShip = new Ship(shipData1);
+  const expectedBoard = {
+    x1y1: { ship: expectedShip },
+    x2y1: { ship: expectedShip },
+    x3y1: { ship: expectedShip },
+    x1y2: { isAttacked: true },
+  };
+  const expectedShips = { ship1: expectedShip };
+
+  expect(gameboard.receiveAttack(attackCoordinate)).toEqual("miss");
+  expect(gameboard.boardDatabase).toEqual(expectedBoard);
+  expect(gameboard.shipsDatabase).toEqual(expectedShips);
+});
+
+test("gameboard duplicate recieve attack ship", () => {
+  let gameboard = new Gameboard();
+  let shipData1 = { name: "ship1", coordinates: ["x1y1", "x2y1", "x3y1"] };
+  const attackCoordinate = "x1y1";
+
+  gameboard.placeShip(shipData1);
+
+  const expectedShip = new Ship(shipData1);
+  expectedShip.hit(attackCoordinate);
+  const expectedBoard = {
+    x1y1: { isAttacked: true, ship: expectedShip },
+    x2y1: { ship: expectedShip },
+    x3y1: { ship: expectedShip },
+  };
+  const expectedShips = { ship1: expectedShip };
+
+  expect(gameboard.receiveAttack("x1y1")).toEqual("hit");
+  expect(gameboard.receiveAttack("x1y1")).toEqual("error");
+  expect(gameboard.boardDatabase).toEqual(expectedBoard);
+  expect(gameboard.shipsDatabase).toEqual(expectedShips);
+});
+
+test("gameboard duplicate recieve attack empty", () => {
+  let gameboard = new Gameboard();
+  let shipData1 = { name: "ship1", coordinates: ["x1y1", "x2y1", "x3y1"] };
+  let attackCoordinate = "x1y2";
+
+  gameboard.placeShip(shipData1);
+
+  const expectedShip = new Ship(shipData1);
+  const expectedBoard = {
+    x1y1: { ship: expectedShip },
+    x2y1: { ship: expectedShip },
+    x3y1: { ship: expectedShip },
+    x1y2: { isAttacked: true },
+  };
+  const expectedShips = { ship1: expectedShip };
+
+  expect(gameboard.receiveAttack(attackCoordinate)).toEqual("miss");
+  expect(gameboard.receiveAttack(attackCoordinate)).toEqual("error");
+  expect(gameboard.boardDatabase).toEqual(expectedBoard);
+  expect(gameboard.shipsDatabase).toEqual(expectedShips);
 });
