5905a07af1a163b40909e57152f98dce27e6d196
Tzun Wong <tzun.wong@gmail.com>
2022-06-15 14:55:00 +0800

    reconfig webpack plugins; rearranged dist; added favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
 module.exports = {
   mode: "production",
   entry: "./src/index.js",
   output: {
-    filename: "js/main.js",
+    filename: "js/main.bundle.js",
     path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "assets/[name][ext]",
+    clean: true,
   },
   module: {
     rules: [
@@ -20,5 +24,13 @@ module.exports = {
   optimization: {
     minimizer: [new CssMinimizerPlugin()],
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./css/[name].css",
+    }),
+    new FaviconsWebpackPlugin("./src/img/icons8-sailboat-48.png"),
+  ],
 };
